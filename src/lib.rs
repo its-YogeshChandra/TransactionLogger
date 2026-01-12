42c6f02241d0edcdc00bbb65cec60591093d38d5
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-13 03:31:27 +0530

    function logging to the txt file for now

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,11 @@ use solana_geyser_plugin_interface::geyser_plugin_interface::{
 use solana_program::pubkey::Pubkey;
 use std::sync::{Arc, Mutex};
 mod config;
+mod database;
 mod plugin;
 use config::makeconfig;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 
 const USDC_MINT: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 // the main brain struct
@@ -19,6 +22,7 @@ pub struct DataStruct {
     last_successful_update_time: u8,
     error_count: u8,
     total_transaction_processed: u8,
+    txlog_file: Option<File>,
 }
 
 use solana_program::clock::Slot;
@@ -46,6 +50,18 @@ impl GeyserPlugin for MainState {
 
         //add the database_url to the main state struct
         self.state.lock().unwrap().database_url = data.database_url;
+
+        // 1. Open the file ONCE
+        let file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("whale_alerts.txt")
+            .map_err(|e| GeyserPluginError::ConfigFileReadError { msg: e.to_string() })?;
+
+        // 2. Save it into your state
+        let mut state = self.state.lock().unwrap();
+        state.txlog_file = Some(file);
+
         Ok(())
     }
 
@@ -58,7 +74,7 @@ impl GeyserPlugin for MainState {
         let data = match transaction {
             ReplicaTransactionInfoVersions::V0_0_1(info) => {}
             ReplicaTransactionInfoVersions::V0_0_2(info) => {
-                self::MainState::process_manager(&info);
+                self::MainState::process_manager(self, &info);
             }
         };
 
@@ -67,7 +83,7 @@ impl GeyserPlugin for MainState {
 }
 
 impl MainState {
-    pub fn process_manager(info: &ReplicaTransactionInfoV2) {
+    pub fn process_manager(&self, info: &ReplicaTransactionInfoV2) {
         let meta = info.transaction_status_meta;
         if let Some(post_balance) = &meta.post_token_balances {
             //loop the post balance
@@ -94,6 +110,20 @@ impl MainState {
                             let accounts_keys = info.transaction.message().account_keys();
                             let owner_address =
                                 accounts_keys.get(balance.account_index as usize).unwrap();
+
+                            //append data to a txt file -- for now --
+                            let message = format!(
+                                "WHALE ALERT | Slot: {} | Tx: {:?} | User: {:?} | Amount: {} USDC\n",
+                                0, // You can pass 'slot' to this function if you want precise slot numbers
+                                info.signature,
+                                owner_address,
+                                diff / 1_000_000
+                            );
+
+                            let mut state = self.state.lock().unwrap();
+                            if let Some(file) = &mut state.txlog_file {
+                                let _ = file.write_all(message.as_bytes());
+                            }
 
                             println!(
                                 "ALERT! USER : {:?} |  RECEIVED: {} USDC",
